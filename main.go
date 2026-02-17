3f65c5060229792a2213ff693e4939266285e9e8
adauri.developer <adauri.developer@gmail.com>
2026-02-17 18:23:25 -0300

    modified  main.go, adicionado comentarios

--- main.go
+++ main.go
@@ -5,15 +5,14 @@ import (
 	"net/http"
 )
 
-// TODO urlDatabase
+// Nosso "banco de dados" temporario da memoria
 var urlDatabase = map[string]string{
 	"google": "https://www.google.com",
 	"github": "https://github.com/adaurideveloper-sudo",
 }
 
-// TODO http.HandleFunc
 func main() {
-
+	//Rota para encurtar
 	http.HandleFunc("/", redirect)
 
 	fmt.Println("Servidor rodando em http://localhost:8080")
@@ -21,9 +20,9 @@ func main() {
 
 }
 
-// TODO func redirect
 func redirect(w http.ResponseWriter, r *http.Request) {
-
+	//r.URL.Path traz algo como "/google"
+	// Usamos [1:] para tirar a primeira barra e so ficar so comn "google"
 	code := r.URL.Path[1:]
 
 	fmt.Printf("Buscamos pelo codigo: '%s'\n", code)
@@ -33,12 +32,15 @@ func redirect(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//Busca no mapa
 	originalURL, exists := urlDatabase[code]
 
 	if exists {
+		// Se existir, mandamos o usuario para la (Status 302: Found)
 		fmt.Printf("Sucesso! Redirecionado para: %s\n, code")
 		http.Redirect(w, r, originalURL, http.StatusFound)
 	} else {
+		//Se nao existir, avisamos
 		fmt.Printf("erro: O codigo '%s' nao existe no mapa \n", code)
 		http.Error(w, "Link nao encontrado! verifique se digitou corretamente", http.StatusNotFound)
 
